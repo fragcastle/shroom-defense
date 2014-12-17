9506391bf09f7c4bb53738d4b8f588ef246c0898
John Bubriski <bubriski@gmail.com>
2014-12-16 23:11:22 -0500

    Yep, explosions

--- Assets/EnemyBase.cs
+++ Assets/EnemyBase.cs
@@ -4,12 +4,18 @@ using System.Collections;
 public class EnemyBase : MonoBehaviour
 {
 	public int Health = 100;
+	
+	public Transform DeathEffect;
 
 	public void ApplyDamage(int damage)
 	{
 		Health -= damage;
 		
 		if (Health <= 0)
+		{
+			Instantiate(DeathEffect, transform.position, Quaternion.identity);
+            
 			Destroy(gameObject);
+		}
 	}
 }
